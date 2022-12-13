7f94971e66da637150cb20b92b526eaf06896d79
C191239 <zsz2007314zsz@sina.com>
2022-12-13 22:40:03 +0800

    Initial commit.

---
+++ src/main/java/org/c191239/firechargeutil/FireChargeUtil.java
@@ -0,0 +1,10 @@
+package org.c191239.firechargeutil;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FireChargeUtil implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
